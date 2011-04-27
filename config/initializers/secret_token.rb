5e2211b896eae446e248bdbca5566c518b96b394
Pragnesh Vaghela <admin@technologythree.com>
2011-04-26 22:25:30 -0500

    pushing rails basic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IPhoneDemo::Application.config.secret_token = '16fa0349636fc0bec38b51dbb811ee8475fb985b2c775d1ba84b455d5c9502d25f7ab0ed73bd574ffb7168ed38767fe47fc651e9e0ea0a6182be7de3869cf52b'
