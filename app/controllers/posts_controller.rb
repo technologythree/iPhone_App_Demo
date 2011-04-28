5a0c14fadc24712279e917e1cc23ba07332ef5d9
Pragnesh Vaghela <admin@technologythree.com>
2011-04-27 23:39:43 -0500

    updated to use jquery mobile and associated views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
+      format.mobile # index.html.erb
       format.xml  { render :xml => @posts }
     end
   end
@@ -17,6 +18,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
+      format.mobile # show.mobile.erb 
       format.xml  { render :xml => @post }
     end
   end
@@ -28,6 +30,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
+      format.mobile # new.mobile.erb
       format.xml  { render :xml => @post }
     end
   end
@@ -45,9 +48,11 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to(@post, :notice => 'Post was successfully created.') }
+        format.mobile { redirect_to(@post, :notice => 'Post was successfully created.') }
         format.xml  { render :xml => @post, :status => :created, :location => @post }
       else
         format.html { render :action => "new" }
+        format.mobile { render :action => "new" }
         format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
       end
     end
@@ -61,9 +66,11 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.update_attributes(params[:post])
         format.html { redirect_to(@post, :notice => 'Post was successfully updated.') }
+        format.mobile { redirect_to(@post, :notice => 'Post was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
+        format.mobile { render :action => "edit" }
         format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
       end
     end
@@ -77,6 +84,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to(posts_url) }
+      format.mobile { redirect_to(posts_url) }
       format.xml  { head :ok }
     end
   end
