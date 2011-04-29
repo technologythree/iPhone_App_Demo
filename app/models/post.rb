3499967149e7dce44dd7078c92405fb2b3acfed7
Pragnesh Vaghela <admin@technologythree.com>
2011-04-29 02:21:30 -0500

    cleaned up ui

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :name,  :presence => true
   validates :title, :presence => true,
                       :length => { :minimum => 5 }
-  has_many :comments, :dependent => :destroy
+  has_many :comments, :dependent => :destroy, :order => "created_at DESC"
 end
